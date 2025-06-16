c912bb42ea742792262125683d7e75e27e560477
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-06-16 18:46:23 +0600

    full donation section done, lastly 2nd and 3rd remaining box have been done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-
-
+// START 1ST BOX JS SECTION
 document.getElementById('donate-noakhali').addEventListener('click', function(){
 
     const donateInputNoakhali = document.getElementById('donate-input-noakhali').value;
@@ -18,8 +17,8 @@ document.getElementById('donate-noakhali').addEventListener('click', function(){
          const newMyBalance = parseMyBalance - parseDonateInputNoakhali;
          document.getElementById('my-balance').innerText = newMyBalance;
          
-        const modalMoney = document.getElementById('modal-money');
-        modalMoney.innerHTML += `<p>You have donated ${donateInputNoakhali} and total ${newBalanceNoakhali}</p>`
+        const modalMoneyNoakhali = document.getElementById('modal-money-noakhali');
+        modalMoneyNoakhali.innerHTML += `<p>You have donated ${donateInputNoakhali} and total ${newBalanceNoakhali}</p>`
 
         
     }
@@ -28,3 +27,70 @@ document.getElementById('donate-noakhali').addEventListener('click', function(){
     }
 
 })
+// END 1ST BOX JS SECTION
+
+// START 2ND BOX JS SECTION
+document.getElementById('donate-feni').addEventListener('click', function(){
+
+    const donateInputfeni = document.getElementById('donate-input-feni').value;
+    const parseDonateInputfeni = parseFloat(donateInputfeni);
+
+    const balancefeni = document.getElementById('balance-feni').innerText;
+    const parseBalancefeni = parseFloat(balancefeni);
+
+    const myBalance = document.getElementById('my-balance').innerText;
+    const parseMyBalance = parseFloat(myBalance);
+
+    if(parseDonateInputfeni > 0 && !isNaN(parseDonateInputfeni)){
+         const newBalancefeni = parseDonateInputfeni + parseBalancefeni;
+         document.getElementById('balance-feni').innerText = newBalancefeni;
+
+         const newMyBalance = parseMyBalance - parseDonateInputfeni;
+         document.getElementById('my-balance').innerText = newMyBalance;
+         
+        const modalMoneyfeni = document.getElementById('modal-money-feni');
+        modalMoneyfeni.innerHTML += `<p>You have donated ${donateInputfeni} and total ${newBalancefeni}</p>`
+
+        
+    }
+    else{
+        alert('Kindly Enter amount greater than 0 and only number');
+    }
+
+})
+// END 2ND BOX JS SECTION
+
+
+
+// START 3RD BOX JS SECTION
+document.getElementById('donate-quotaAid').addEventListener('click', function(){
+
+    const donateInputquotaAid = document.getElementById('donate-input-quotaAid').value;
+    const parseDonateInputquotaAid = parseFloat(donateInputquotaAid);
+
+    const balancequotaAid = document.getElementById('balance-quotaAid').innerText;
+    const parseBalancequotaAid = parseFloat(balancequotaAid);
+
+    const myBalance = document.getElementById('my-balance').innerText;
+    const parseMyBalance = parseFloat(myBalance);
+
+    if(parseDonateInputquotaAid > 0 && !isNaN(parseDonateInputquotaAid)){
+         const newBalancequotaAid = parseDonateInputquotaAid + parseBalancequotaAid;
+         document.getElementById('balance-quotaAid').innerText = newBalancequotaAid;
+
+         const newMyBalance = parseMyBalance - parseDonateInputquotaAid;
+         document.getElementById('my-balance').innerText = newMyBalance;
+         
+        const modalMoneyquotaAid = document.getElementById('modal-money-quotaAid');
+        modalMoneyquotaAid.innerHTML += `<p>You have donated ${donateInputquotaAid} and total ${newBalancequotaAid}</p>`
+
+        
+    }
+    else{
+        alert('Kindly Enter amount greater than 0 and only number');
+    }
+
+})
+// END 3RD BOX JS SECTION
+
+
